a0fe567c622beaab013e7e50fad199da1a3e7a9e
Jacob Zamora <rjzamora96@gmail.com>
2016-11-01 09:23:01 -0600

    Mini-commit

---
+++ Editor/PreviewItem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Editor
+{
+    public class PreviewItem
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Rotation { get; set; }
+        public double Scale { get; set; }
+        public Image Sprite { get; set; }
+        public EntityItem Owner { get; set; }
+        public void Update()
+        {
+
+        }
+    }
+}
